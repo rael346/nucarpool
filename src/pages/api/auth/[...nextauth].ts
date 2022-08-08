823646a4d9c9d5fe33116799bbd62f77b3605a4a
Duy Tran <duyvutran2303@gmail.com>
2022-08-08 12:37:23 -0400

    fix: change dependency version for next-auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -22,6 +22,9 @@ export const authOptions: NextAuthOptions = {
       clientSecret: serverEnv.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  pages: {
+    signIn: "/sign-in",
+  },
 };
 
 export default NextAuth(authOptions);
